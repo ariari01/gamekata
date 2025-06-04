e847810c94d0bb6f4cebeb17a91ea2927b5e3b43
ariari01 <juh.shin@gmail.com>
2025-06-04 13:37:46 +0900

    feature/25

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void createGame() {
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess(null);
+        });
+    }
 }
