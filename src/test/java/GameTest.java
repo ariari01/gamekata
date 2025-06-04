48839163909bcf5ff0bf977035ca1290112fba0f
ariari01 <juh.shin@gmail.com>
2025-06-04 14:02:49 +0900

    refactoring/34

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -18,23 +18,20 @@ class GameTest {
         assertNotNull(game);
     }
 
-    @Test
-    @DisplayName("입력이 없으면 예외처리")
-    void throwExceptionWhenInputIsNull() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            game.guess(null);
-        });
+    private void assertIllegalArgument(String guessNumber) {
+        try {
+            game.guess(guessNumber);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 
     @Test
     @DisplayName("입력 숫자값이 3자리가 아니면 예외처리")
-    void throwExceptionWhenInputLengthIsUnmatched() {
-        try {
-            game.guess("12");
-            fail();
-        }
-        catch (IllegalArgumentException e){
-
-        }
+    void throwIllegalArgumentExceptionInvalidInput() {
+        assertIllegalArgument(null);
+        assertIllegalArgument("12");
+        assertIllegalArgument("1234");
     }
 }
